45558cbb0de372db8fc5cb5a7618d89c3a725c18
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2022-12-12 20:30:55 -0500

    Updated questions and display feedback
    Completed chapter 4 & 5

---
+++ script.js
@@ -0,0 +1,95 @@
+const question= document.getElementById("questions");
+/* You can create a snippet for quicker code */
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Which choice is part of fullstack wed dev?",
+        choice1: "HTML",
+        choice2: "Python",
+        choice3: "C#",
+        choice4: "Java",
+        answer: 1
+
+    },
+    {
+        question: "Which choice one makes fullstack web development prettier?",
+        choice1: "HTML",
+        choice2: "Python",
+        choice3: "CSS",
+        choice4: "Java",
+        answer: 3
+
+    },
+    {
+        question: "Which one is not a programming language?",
+        choice1: "Java",
+        choice2: "Python",
+        choice3: "C#",
+        choice4: "HTML",
+        answer: 4
+
+    },
+]
+
+//constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign("/end.html");
+    };
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+// These two codes do the same thing. This one is mre readable
+        /*const classToApply = 'incorrect';
+        if (selectedAnswer == currentQuestion.answer) {
+            classToApply = 'correct';
+        };*/
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+startGame();
