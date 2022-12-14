15322c5db54751df7bad4982f9243a228fbf0b08
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2022-12-13 20:55:11 -0500

    Finished Chapter 6
    HUD - QuestionCounter and Score

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const question= document.getElementById("questions");
 /* You can create a snippet for quicker code */
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -54,6 +56,10 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     };
     questionCounter++;
+    questionCounterText.innerText = questionCounter + '/' + MAX_QUESTIONS; 
+    /* A better to do the same above code in ES6 is with the below code */
+    /*questionCounterText.innerText = '${questionCounter}/${MAX_QUESTIONS}';*/
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -83,6 +89,10 @@ choices.forEach(choice => {
 
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout( () => {
@@ -91,5 +101,10 @@ choices.forEach(choice => {
         }, 1000);
     });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
 
 startGame();
