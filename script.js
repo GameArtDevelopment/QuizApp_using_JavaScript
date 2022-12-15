185546c2e1fa0a5aca64dd6f47a05f389372c23d
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2022-12-14 22:15:57 -0500

    Created a progressBar
    I adjusted my code to match my version of JavaScript. I will need to research how to update to ES6.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const question= document.getElementById("questions");
 /* You can create a snippet for quicker code */
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -56,9 +57,11 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     };
     questionCounter++;
-    questionCounterText.innerText = questionCounter + '/' + MAX_QUESTIONS; 
+    progressText.innerText = 'Question ' + questionCounter + '/' + MAX_QUESTIONS; 
     /* A better to do the same above code in ES6 is with the below code */
     /*questionCounterText.innerText = '${questionCounter}/${MAX_QUESTIONS}';*/
+    // Update the progressBarFull
+    progressBarFull.style.width = (questionCounter / MAX_QUESTIONS) * 100 + "%";
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
